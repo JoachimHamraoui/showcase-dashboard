ebdee577e243a8b6aac8149dea242ab3e2f8a33b
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-11-05 21:48:07 +0100

    config: plugin and session config for better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,19 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/drizzle/db"; // your drizzle instance
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    session: {
+        cookieCache: {
+            enabled: true,
+            maxAge: 60 * 60 * 24 * 30 // 30 days
+        }
+    },
+    plugin: [nextCookies()],
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
