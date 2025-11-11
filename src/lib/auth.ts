832e97198f8eb6a7bb01ce28ab8aa3f66f77370c
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-11-11 13:52:29 +0100

    feat(auth): config for google oauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,17 +4,23 @@ import { db } from "@/drizzle/db"; // your drizzle instance
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-    emailAndPassword: {
-        enabled: true,
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
-    session: {
-        cookieCache: {
-            enabled: true,
-            maxAge: 60 * 60 * 24 * 30 // 30 days
-        }
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60 * 24 * 30, // 30 days
     },
-    plugin: [nextCookies()],
-    database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
-    }),
+  },
+  plugin: [nextCookies()],
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite"
+  }),
 });
