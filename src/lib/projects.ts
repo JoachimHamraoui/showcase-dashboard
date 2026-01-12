3531403a56345d04a835b434fcd068bc16dab2c0
JoachimHamraoui <joachimhamraoui@gmail.com>
2026-01-12 21:08:40 +0100

    refactor: retrieve project and then return only if user's id matches request's id

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -3,15 +3,15 @@ import { project } from "@/drizzle/schema";
 import { eq } from "drizzle-orm";
 
 export async function getProjects(userId: string) {
-  return await db
-    .select()
-    .from(project)
-    .where(eq(project.userId, userId));
+  return await db.select().from(project).where(eq(project.userId, userId));
 }
 
 export async function getProjectById(projectId: string, userId: string) {
- return await db
+  const result = await db
     .select()
     .from(project)
-    .where((eq(project.id, projectId), eq(project.userId, userId)));
+    .where(eq(project.id, projectId));
+
+  // Return first result if it belongs to the user, otherwise null
+  return result[0]?.userId === userId ? result[0] : null;
 }
