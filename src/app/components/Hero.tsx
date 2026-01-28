cbe6b96fecb7d8fede9783b7ce7b352a86419395
JoachimHamraoui <joachimhamraoui@gmail.com>
2026-01-28 11:12:12 +0100

    feat(home): hero section implemented

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { AuroraText } from "@/components/ui/aurora-text";
+import { AnimatedShinyText } from "@/components/ui/animated-shiny-text";
+import Link from "next/link";
+import { InteractiveHoverButton } from "@/components/ui/interactive-hover-button";
 
 export function Hero() {
   return (
     <div className="w-full h-full relative flex flex-col items-center justify-center py-12 lg:py-32">
-      <p className="text-lg text-muted-foreground">Welcome to</p>
+        <AnimatedShinyText>✨ Introducing Alpha v1.0</AnimatedShinyText>
       <h1 className="-mt-2 font-black text-4xl sm:text-6xl md:text-7xl lg:text-9xl tracking-tighter">
         <AuroraText speed={1} colors={["#FE8C00", "#F83600"]}>
           DSHBRD.
@@ -13,9 +16,12 @@ export function Hero() {
       <p className="text-md font-medium text-muted-foreground">
         Manage your portfolio&apos;s projects right here.
       </p>
-      <p className="text-md font-medium text-muted-foreground">
+      <p className="text-md font-medium text-muted-foreground ùm">
         For developers, by developers.
       </p>
+      <Link href="/auth/login" className="mt-6 -mb-6">
+        <InteractiveHoverButton>Get Started</InteractiveHoverButton>
+      </Link>
       <Image
         src="/hero.png"
         alt="Hero Image"
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,14 +1,20 @@
-import { RainbowButton } from "@/components/ui/rainbow-button";
+import { AuroraText } from "@/components/ui/aurora-text";
+import { Button } from "@/components/ui/button";
+import { User2Icon } from "lucide-react";
 import Link from "next/link";
 
 export function NavBar() {
   return (
     <nav className="flex items-center justify-between py-5">
       <Link href="/">
-        <h1 className="text-3xl font-black text-primary hover:text-primary-600">DSHBRD.</h1>
+        <h1 className="text-3xl font-black hover:text-primary-600">
+          <AuroraText speed={1} colors={["#FE8C00", "#F83600"]}>
+            DSHBRD.
+          </AuroraText>
+        </h1>
       </Link>
       <Link href="/auth/login">
-        <RainbowButton>Get Started</RainbowButton>
+        <Button><User2Icon className="size-4" /> Login</Button>
       </Link>
     </nav>
   );
